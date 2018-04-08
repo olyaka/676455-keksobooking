e2faad556c73af52d1f7b04b35c2e7525823be63
Olga <fej-d@yandex.ru>
2018-04-08 12:42:19 +0300

    Исправлены селекторы

--- js/map.js
+++ js/map.js
@@ -79,23 +79,22 @@ var similarPinTemplate = document.querySelector('template').content.querySelecto
 
 var renderPin = function (notice) {
   var pinElement = similarPinTemplate.cloneNode(true);
-  var pinX = notice.location.x + 20;
-  var pinY = notice.location.y + 40;
+  var pinX = notice.location.x - 25;
+  var pinY = notice.location.y - 70;
   pinElement.style = 'left: ' + pinX + 'px; top: ' + pinY + 'px;';
   pinElement.querySelector('.map__pin img').src = notice.author.avatar;
   pinElement.querySelector('.map__pin img').alt = notice.offer.title;
   return pinElement;
 };
 
-var fragment = document.createDocumentFragment();
-
+var pinFragment = document.createDocumentFragment();
 for (i = 0; i < notices.length; i++) {
-  fragment.appendChild(renderPin(notices[i]));
+  pinFragment.appendChild(renderPin(notices[i]));
 }
-similarPinsElement.appendChild(fragment);
+similarPinsElement.appendChild(pinFragment);
 
 var similarCardTemplate = document.querySelector('template').content.querySelector('.map__card');
-var cardElement = document.querySelector('.map__card');
+var similarCardsElement = document.querySelector('.map');
 
 var renderCard = function (notice) {
   var accomodationType;
@@ -115,10 +114,9 @@ var renderCard = function (notice) {
   cardTemplate.querySelector('.popup__type').textContent = accomodationType;
   cardTemplate.querySelector('.popup__text--capacity').textContent = notice.offer.rooms + ' комнаты для ' + notice.offer.guests + ' гостей';
   cardTemplate.querySelector('.popup__text--time').textContent = 'Заезд после ' + notice.offer.checkin + ', выезд до ' + notice.offer.checkout;
-  // cardTemplate.querySelector('.popup__features').appendChild = notice.offer.address;
   cardTemplate.querySelector('.popup__description').textContent = notice.offer.description;
 
-  var featureElementTemplate = document.querySelector('li');
+  var featureElementTemplate = document.querySelector('template').content.querySelector('.popup__feature');
   var featureFragment = document.createDocumentFragment();
 
   while (cardTemplate.querySelector('.popup__feature')) {
@@ -136,20 +134,21 @@ var renderCard = function (notice) {
 
   cardTemplate.querySelector('.popup__avatar').src = notice.author.avatar;
 
-  var photoElementTemplate = document.querySelector('template').content.querySelector('.popup__photos img');
+  var photoElementTemplate = document.querySelector('template').content.querySelector('.popup__photo');
   var photoFragment = document.createDocumentFragment();
 
-  for (i = 0; i < notice.offer.photos.length; i++) {
+  for (i = 1; i < notice.offer.photos.length; i++) {
     var photoElement = photoElementTemplate.cloneNode(true);
     photoElement.src = notice.offer.photos[i];
     photoFragment.appendChild(photoElement);
   }
 
+  cardTemplate.querySelector('.popup__photos img').src = notice.offer.photos[0];
   cardTemplate.querySelector('.popup__photos').appendChild(photoFragment);
-  //
+
   return cardTemplate;
 };
 
-var fragment1 = document.createDocumentFragment();
-fragment1.appendChild(renderCard(notices[1]));
-cardElement.appendChild(fragment1);
+var cardFragment = document.createDocumentFragment();
+cardFragment.appendChild(renderCard(notices[0]));
+similarCardsElement.appendChild(cardFragment);
