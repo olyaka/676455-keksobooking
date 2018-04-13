1f26d5793424e0b34a665f0801abdf51ca1d8209
Olga <fej-d@yandex.ru>
2018-04-13 16:43:29 +0300

    Заполнение и валидация формы

--- js/map.js
+++ js/map.js
@@ -214,3 +214,44 @@ var onMapPinClick = function (evt) {
     }
   }
 };
+
+var selectedTypeElement = document.querySelector('#type');
+var inputPrice = document.querySelector('#price');
+
+selectedTypeElement.addEventListener('change', function (evt) {
+  if (evt.currentTarget.value === 'bungalo') {
+    inputPrice.placeholder = '0';
+    inputPrice.min = 0;
+  } else if (evt.currentTarget.value === 'flat') {
+    inputPrice.placeholder = '1000';
+    inputPrice.min = 1000;
+  } else if (evt.currentTarget.value === 'house') {
+    inputPrice.placeholder = '5000';
+    inputPrice.min = 5000;
+  } else if (evt.currentTarget.value === 'palace') {
+    inputPrice.placeholder = '10000';
+    inputPrice.min = 10000;
+  }
+});
+
+var selectedCheckIn = document.querySelector('#timein');
+var selectedCheckOut = document.querySelector('#timeout');
+
+selectedCheckIn.addEventListener('change', function (evt) {
+  selectedCheckOut.value = evt.currentTarget.value;
+});
+
+selectedCheckOut.addEventListener('change', function (evt) {
+  selectedCheckIn.value = evt.currentTarget.value;
+});
+
+var selectedRoomNumber = document.querySelector('#room_number');
+var selectedCapacity = document.querySelector('#capacity');
+
+selectedRoomNumber.addEventListener('change', function (evt) {
+  selectedCapacity.value = evt.currentTarget.value;
+});
+
+selectedCapacity.addEventListener('change', function (evt) {
+  selectedRoomNumber.value = evt.currentTarget.value;
+});
